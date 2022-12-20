8f3d4737f2956bd2f22a7f811d63d763444af1b1
suryakant thombare <suryakant.thombare@ispace.com>
2022-12-20 13:12:02 +0530

    get network

--- src/App.js
+++ src/App.js
@@ -4,24 +4,29 @@ import CheckBalance from "./components/checkBalance";
 import TransactionDetails from "./components/transactionDetails";
 import CreateAccount from "./components/createAccount";
 import { getBlockData } from "./functions/blockData";
+import { getNetwork } from "./functions/network";
 
 
 function App() {
 	const [blockDetails, setBlockDetails] = useState([])
+	const [network, setNetwork] = useState()
 
 	useEffect(() => {
 		const getData = async () => {
 			const data =await getBlockData()
-			console.log(data);
+			const network = await getNetwork()
+			console.log(network);
 			setBlockDetails(data)
+			setNetwork(network)
 		  }; 
 		  getData()
 	}, [])
 
 	return (
 		<div className="App">
-			<CreateAccount />
+			<h3>Netrok type: {network}</h3>
 			<ReactJson theme='tomorrow' src={blockDetails} />
+			<CreateAccount />
 			<CheckBalance />
 			<TransactionDetails />
 		</div>
--- src/functions/network.js
+++ src/functions/network.js
@@ -1,6 +1,6 @@
 import {web3Connect} from "./web3connect";
 
-export const getBalance = async () => {
+export const getNetwork = async () => {
     let network = await web3Connect.eth.net.getNetworkType();
     console.log(network);
     return network
